e16bfb1df68b0bf22972e4231e6573a6b37cdd6f
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-05-31 02:12:17 +0600

    fixe

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+import { nextui } from "@nextui-org/theme";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layout/**/*.{js,ts,jsx,tsx,mdx}",
+    "./screens/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      container: "1280px",
+      xl: { max: "1279px" },
+      lg: { max: "900px" },
+      md: { max: "768px" },
+      sm: { max: "640px" },
+      xs: { max: "440px" },
+      ls: { max: "320px" },
+    },
+    extend: {
+      colors: {},
+      backgroundImage: {},
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
