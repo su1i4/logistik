ff0f4c367e130bb3a3ce87ed212f8aef96d6d51a
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-06-18 16:44:42 +0600

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="ru">
       <head>
-        <link rel="icon" href="../public/delievery-truck.png" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
